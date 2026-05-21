99870b394a27bc1c7812875d08a03edaa1b6c87a
Samuel Nwaokocha <81534141+ItzRealsam@users.noreply.github.com>
2026-05-21 01:06:22 +0100

    delete product from checkout HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -16,7 +16,8 @@ cart.forEach((cartItem) => {
 
   cartSummaryHTML +=
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container
+    js-cart-item-container-${matchingItem.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -39,7 +40,9 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link 
+              js-delete-quantity-link link-primary"
+              data-product-id = "${matchingItem.id}">
               Delete
             </span>
           </div>
@@ -97,3 +100,16 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-quantity-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const { productId } = link.dataset;
+      removeFromCart(productId);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
+    })
+
+    
+  })
